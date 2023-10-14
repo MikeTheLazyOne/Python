f78f3e6b1076c186d2a57b2c83495c49d257ec1c
MikeTheStudent <01161985@pw.edu.pl>
2023-10-14 21:27:40 +0200

    finished idea of changing txt filse

--- day-24/main.py
+++ day-24/main.py
@@ -2,5 +2,18 @@
 #     content = file.read()
 #     print(content)
 
-with open("my_text(1).txt", "w") as file:
-    file.write("\nNew text.")
+with open("names.txt", "r") as file:
+    names = file.readlines()
+    for name in names:
+        with open(f"list_do_{name}.txt", "w") as msg:
+            with open("my_text.txt", "r")as txt:
+                lines = txt.readlines()
+                for line in lines:
+                    tekst = line
+                    if "[name]" in tekst:
+                        tekst = tekst.replace("[name]", name)
+                        tekst = tekst.rstrip()
+                        tekst = tekst + "\n"
+                        print(tekst)
+                    msg.write(tekst)
+    
