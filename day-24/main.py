06eaa84c1f466e821ca4c4f9ef895a08b1020b9f
MikeTheStudent <01161985@pw.edu.pl>
2023-10-14 13:55:21 +0200

    test of how writing to files works

--- day-24/main.py
+++ day-24/main.py
@@ -1,1 +1,6 @@
-file = open("my_text.tzt")
+# with open("my_text.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open("my_text(1).txt", "w") as file:
+    file.write("\nNew text.")
